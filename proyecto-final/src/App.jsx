160e60e93c6cdd063d3711ad5ef55edf576e8c99
efalbrigi <efalbrigi@gmail.com>
2022-04-18 19:51:24 -0300

    cambios

--- proyecto-final/src/App.jsx
+++ proyecto-final/src/App.jsx
@@ -1,6 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
---
+++ proyecto-final/src/components/NavBar/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function navbar(props) {
+    return (
+        <div>
+            este es mi proyecto
+        </div>
+    );
+}
+
+export default navbar;
